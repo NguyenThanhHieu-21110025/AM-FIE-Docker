b6fb7b5389cb5f223f0f4e80ff1c9014f703a722
Nguyen Thanh Hieu <thanhhieu@Hieu-nho.local>
2025-05-09 19:06:16 +0700

    update route export

--- server/src/routes/exportDB.Route.js
+++ server/src/routes/exportDB.Route.js
@@ -6,6 +6,11 @@ const exportDB  = require('../Controller/exportDB.Controller');
 const router = express.Router();
 
 
-router.get('/export', exportDB.exportDB);
+// Lấy danh sách các type có sẵn
+router.get('/export/types', exportDB.getAvailableTypes);
+// Export theo type cụ thể
+router.get('/export/:type', exportDB.exportDB);
+// Export nhiều types (POST request với body chứa array types)
+router.post('/export/multiple', exportDB.exportMultipleTypes);
 
 module.exports = router;
